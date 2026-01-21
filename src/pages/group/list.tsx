de9f792b6966031cf6005dfed85f207a71bb7e5f
Mikhail Kormyshov <kormyshov@yandex.ru>
2026-01-21 20:05:08 +0300

    Delete unused import

--- src/pages/group/list.tsx
+++ src/pages/group/list.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-import SlCard from '@shoelace-style/shoelace/dist/react/card';
 import SlIconButton from '@shoelace-style/shoelace/dist/react/icon-button';
 
 import { GroupListContext } from '../../app/App.tsx';
