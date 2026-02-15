d587eb5dd19fffe4b174886f0dc1a5608cba7706
Mikhail Kormyshov <kormyshov@yandex.ru>
2026-02-15 16:46:11 +0300

    Change to just redirect while join to group

--- src/pages/connect.tsx
+++ src/pages/connect.tsx
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
+import SlIconButton from '@shoelace-style/shoelace/dist/react/icon-button';
+
 import { joinToGroup } from '../entities/upload/groups.ts';
 import { GroupUpdateFlagContext } from '../app/App.tsx';
 
@@ -21,6 +23,8 @@ export default function Connect() {
     <>
       <div style={{ background: 'linear-gradient(rgba(0, 255, 127, 0.4), rgba(0, 0, 255, 0.4))', width: '100%', height: '100vh', boxSizing: 'border-box' }}>
         <div style={{ padding: '1rem' }}>
+          <SlIconButton name="arrow-left-circle-fill" label="Back" style={{ fontSize: '1.5rem' }} onClick={()=>navigate('/')} />
+
           <h2>You are successfully joined</h2>
         </div>
       </div>
