8733c6a95b09d813ba9bd26af331e28cf61aebc0
Mikhail Kormyshov <kormyshov@yandex.ru>
2026-02-15 16:45:21 +0300

    Change to just redirect while join to group

--- src/pages/connect.tsx
+++ src/pages/connect.tsx
@@ -16,10 +16,14 @@ export default function Connect() {
 
   joinToGroup(token ? token : "");
   setGroupUpdateFlag(true);
-  navigate('/');
 
   return (
     <>
+      <div style={{ background: 'linear-gradient(rgba(0, 255, 127, 0.4), rgba(0, 0, 255, 0.4))', width: '100%', height: '100vh', boxSizing: 'border-box' }}>
+        <div style={{ padding: '1rem' }}>
+          <h2>You are successfully joined</h2>
+        </div>
+      </div>
     </>
   );
 }
